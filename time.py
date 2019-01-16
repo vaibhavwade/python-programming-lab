026752ce9055b36f7e8e22979c13fb8104a667c4
vaibhavwade <46518366+vaibhavwade@users.noreply.github.com>
2019-01-16 14:01:47 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,14 @@
+x=int(input('enter time in hour '))
+y=x*30
+print(y)
+z=int(input('enter time in minute '))
+p=z/2
+print('number degree ')
+print(p)
+v=p+y
+print(v)
+c=int(input('enter time in second '))
+k=c/20
+print(k)
+f=p+y+k
+print(f)
